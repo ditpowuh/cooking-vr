33caeb40c1079695166be2bbf3cc27ae335d8c02
ditpowuh <ditpowuh@gmail.com>
2022-11-20 16:00:03 +1100

    Clean up + Finish website

--- Assets/Scripts/First Person/FirstPersonMovement.cs
+++ Assets/Scripts/First Person/FirstPersonMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FirstPersonMovement : MonoBehaviour {
 
@@ -40,6 +41,9 @@ public class FirstPersonMovement : MonoBehaviour {
 			}
 			if (Input.GetKey(KeyCode.Escape) && Input.GetKey(KeyCode.Space)) {
 				Application.Quit();
+			}
+			if (Input.GetKeyDown(KeyCode.R)) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 			}
     }
 
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## The Cooking Simulator: A VR Project (Prototype)
 
 This is both the project files and code for the website that was made.
+'docs' folder is for the website. The rest of the files and folders are for the Unity Project.
